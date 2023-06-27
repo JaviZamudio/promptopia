87d05d0d637786e873d3ab0078470c0bb113df8e
JaviZamudio <javier.elihu100@gmail.com>
2023-06-27 01:34:31 -0600

    try1

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="">
+        Discover and Share
+        <br className="sm:hidden" />
+        <span className="orange_gradient">
+          AI-Powered Prompts
+        </span>
+      </h1>
+      <p>
+        Promptopia is a community for sharing and discovering AI-generated prompts. It's a place to find inspiration for your next creative project, or to share your own ideas with the world.
+      </p>
+    </div>
+  );
+}

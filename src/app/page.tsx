ebeb374f3ad2086908d37e4963431ec1fa67e98c
JaviZamudio <javier.elihu100@gmail.com>
2023-06-27 02:30:52 -0600

    todo ok :)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="">
-        Discover and Share
+      <h1 className="text-5xl font-extrabold text-center mt-4">
+        Discover & Share
         <br className="sm:hidden" />
         <span className="orange_gradient">
           AI-Powered Prompts
         </span>
       </h1>
-      <p>
+      <p className="mt-4 text-zinc-600 text-center max-w-6xl">
         Promptopia is a community for sharing and discovering AI-generated prompts. It's a place to find inspiration for your next creative project, or to share your own ideas with the world.
       </p>
     </div>

e9e47a4d6027e4ee08794097a43b1fa93b6a71fa
JaviZamudio <javier.elihu100@gmail.com>
2023-06-27 01:50:03 -0600

    idk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,22 @@
 import '@/styles/globals.css';
 
 export const metadata = {
-    title: 'Promptopia',
-    description: 'Discover and share AI prompts',
+	title: 'Promptopia',
+	description: 'Discover and share AI prompts',
 };
 
 export default function RootLayout({ children }: any) {
-    return (
-        <html lang="en">
-            <div className="main">
-                <div className="gradient"/>
-            </div>
+	return (
+		<html lang="en">
+			<body>
+				<div className="main">
+					<div className="gradient" />
+				</div>
 
-            <main className="app">
-                {children}
-            </main>
-        </html>
-    );
+				<main className="app">
+					{children}
+				</main>
+			</body>
+		</html>
+	);
 }
